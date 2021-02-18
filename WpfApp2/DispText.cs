cdde6a37f212839aaac2891c8c380e4ce5723fde
KUBO YUKIHIRO 久保 幸弘 <ykubo@se.ritsumei.ac.jp>
2021-02-18 23:45:57 +0900

    プロジェクト ファイルを追加します。

---
+++ WpfApp2/DispText.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    public class DispText : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        public string TTT { get; set; } = "aaa";
+        public string SSS { get; set; } = "bbb";
+
+        public string AAA { get; set; }
+
+        public DispText() { }
+
+        public void SumName()
+        {
+            AAA = TTT + SSS;
+            OnPropertyChanged("AAA");
+        }
+
+
+    }
+
+
+}
